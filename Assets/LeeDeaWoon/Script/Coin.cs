eb6200d3a55e57ad966190e4cd05a43df66f094f
LeeDaeWoon <dleodns1591@naver.com>
2022-10-10 20:40:48 +0900

    자석 아이템 제작
    자석 아이템 제작

---
+++ Assets/LeeDeaWoon/Script/Coin.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Coin : MonoBehaviour
+{
+    [Header("범위")]
+    public float flySpeed; // 날아가는 속도
+    public int coinRange; // 코인범위
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Coin_ColliderRange();
+    }
+
+    //자석 아이템을 먹은 후 
+    public void Coin_ColliderRange()
+    {
+        if (Player.Instance.isMagneting == true)
+        {
+            
+            if (Player.Instance.transform.position.x <= this.transform.position.x + coinRange && Player.Instance.transform.position.y <= this.transform.position.y + coinRange)
+                this.gameObject.transform.DOLocalMove(Player.Instance.transform.position, flySpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            this.gameObject.transform.DOKill();
+            Destroy(this.gameObject);
+        }
+    }
+}
