6a6c010e1a18a6d3a3652ead92a278a3b7395fc0
LeeDaeWoon <dleodns1591@naver.com>
2022-10-12 22:42:18 +0900

    부스터 아이템 제작
    부스터 아이템 제작

--- Assets/LeeDeaWoon/Script/Item.cs
+++ Assets/LeeDeaWoon/Script/Item.cs
@@ -16,7 +16,17 @@ public class Item : PoolingObj
     }
     public EItem eItem;
 
+    public new Collider2D collider2D;
     public SpriteRenderer spriteRenderer;
+    private Vector2 playerDistance;
+
+
+    [Header("아이템 : 부스터")]
+    public float boosterDuration; // 지속시간
+    public float boosterDistance; // 거리
+    public float boosterSpeed; // 연출속도
+    public Ease ease;
+
 
     [Header("아이템 : 자석")]
     public int magnetWaitingTime; // 기다릴 시간
@@ -27,13 +37,15 @@ public class Item : PoolingObj
     [Header("아이템 : 크기조절")]
     public int sizeTime; //커지는 시간
     public int sizeWaitingTime; //기다릴 시간
-    public Vector3 size = new Vector3(); //원하는 사이즈
+    public Vector2 size = new Vector2(); //원하는 사이즈
 
     float sizeTimer;
 
     void Start()
     {
+        collider2D = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        playerDistance.x = Player.Instance.transform.position.x;
     }
 
     void Update()
@@ -56,6 +68,9 @@ public class Item : PoolingObj
     {
         if (collision.CompareTag("Player"))
         {
+            collider2D.enabled = false;
+            spriteRenderer.DOFade(0, 0);
+
             switch (eItem)
             {
                 case EItem.Transformation:
@@ -66,7 +81,6 @@ public class Item : PoolingObj
                 case EItem.Magnet: // 자석
 
                     Player.Instance.isMagneting = true;
-                    spriteRenderer.DOFade(0, 0);
 
                     yield return new WaitForSeconds(magnetWaitingTime);
                     Destroy(this.gameObject);
@@ -78,13 +92,17 @@ public class Item : PoolingObj
 
                 case EItem.Piggybank:
 
-
-                    Destroy(this.gameObject);
-
                     break;
 
 
-                case EItem.Booster:
+                case EItem.Booster: // 부스터
+
+                    Player.Instance.isBoosting = true;
+                    collision.transform.DOLocalMoveX(boosterDistance, boosterSpeed).SetEase(ease);
+                    yield return new WaitForSeconds(boosterDuration);
+                    collision.transform.DOLocalMoveX(playerDistance.x, boosterSpeed).SetEase(ease);
+                    yield return new WaitForSeconds(boosterSpeed);
+                    Destroy(this.gameObject);
                     break;
 
 
@@ -96,7 +114,6 @@ public class Item : PoolingObj
 
                     Player.Instance.isBig = true;
                     collision.transform.DOScale(size, sizeTime);
-                    spriteRenderer.DOFade(0, 0);
                     // 장애물의 콜라이더를 꺼주기
 
                     yield return new WaitForSeconds(sizeWaitingTime);
@@ -104,7 +121,7 @@ public class Item : PoolingObj
                     Player.Instance.isBig = false;
                     sizeTimer = 0;
 
-                    collision.transform.DOScale(new Vector3(1, 1, 1), sizeTime);
+                    collision.transform.DOScale(new Vector2(1, 1), sizeTime);
                     // 장애물의 콜라이더를 켜주기
                     break;
             }
