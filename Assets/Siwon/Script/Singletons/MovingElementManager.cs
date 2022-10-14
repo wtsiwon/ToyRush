b3bb6f55fffb13fac73f3ecf2bcb10e92c4d3138
wtsiwon <siwon4727@gmail.com>
2022-10-14 16:10:34 +0900

    아이템, 장애물, 배경을 저장하는 리스트 만듦

---
+++ Assets/Siwon/Script/Singletons/MovingElementManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingElementManager : MonoBehaviour
+{
+    [Tooltip("현재 나와있는 MovingElements")]
+    public static List<MovingElement> movingElements = new List<MovingElement>();
+    //여기서 장애물 상태조정, 아이템상태, 배경의 속도 등을 조정할 예정
+
+
+
+}
