b546ba8896f5613af3409aff724088e7ac29f5e0
wtsiwon <siwon4727@gmail.com>
2022-10-14 11:21:10 +0900

    점수 증가 장애물 이동 등

--- Assets/Siwon/Script/Obstacle.cs
+++ Assets/Siwon/Script/Obstacle.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Obstacle : PoolingObj
 {
     public EObstacleType obstacleType;
 
+    private Rigidbody2D rb;
+
     private void OnEnable()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        if(obstacleType == EObstacleType.Basic)
+        {
+            rb.velocity = Vector3.left * BackGroundSpawner.Instance.backgroundSpd;
+        }
     }
-
-
-
-
-    
 
 
 }
--- Assets/Siwon/Script/Singletons/GameManager.cs
+++ Assets/Siwon/Script/Singletons/GameManager.cs
@@ -9,6 +9,38 @@ public class GameManager : MonoBehaviour
 
     public bool isBoosting;
     public bool isBig;
-    
-    
+
+    [SerializeField]
+    private bool gameStart;
+    public bool GameStart
+    {
+        get
+        {
+            return gameStart;
+        }
+        set
+        {
+            GameStart = value;
+            if (GameStart == true)
+            {
+
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(UpDate());
+    }
+
+    private IEnumerator UpDate()
+    {
+        if(GameStart == true)
+        {
+            //속도가 빨라지면 점수도 빨리 오름
+            distance += (int)BackGroundSpawner.Instance.backgroundSpd / 10;
+        }
+        yield return StartCoroutine(UpDate());
+    }
+
 }
