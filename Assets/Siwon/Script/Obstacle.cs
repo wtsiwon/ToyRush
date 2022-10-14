f18b0b4fb881ec7728e75022d999b4e267785d1f
wtsiwon <siwon4727@gmail.com>
2022-10-15 01:31:18 +0900

    장애물 움직임 만드는중

--- Assets/Siwon/Script/Obstacle.cs
+++ Assets/Siwon/Script/Obstacle.cs
@@ -10,13 +10,44 @@ public class Obstacle : MovingElement
     protected override void OnEnable()
     {
         base.OnEnable();
-        //obstacleType = GetComponent<EObstacleType>();
+        TypeofDefine();
+    }
+
+    /// <summary>
+    /// enum타입의 따른 정의
+    /// obstacleType에 따라 장애물 
+    /// </summary>
+    private void TypeofDefine()
+    {
         switch (obstacleType)
         {
             case EObstacleType.Basic:
-                
+                //없음
+                break;
+            case EObstacleType.Swing:
+                Swing();
+                break;
+            case EObstacleType.Spin:
+                Spin();
                 break;
         }
+    }
+
+    private void Swing()
+    {
+
+    }
+
+    private void Spin()
+    {
+        StartCoroutine(ISpin());
+    }
+
+    private IEnumerator ISpin()
+    {
+        transform.rotation = new Quaternion(0, 0, 1, 0);
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(ISpin());
     }
 
 
