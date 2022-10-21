ef571ebd85c64aed7ce568ed2ba157690bd14f65
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-10-21 09:03:09 +0900

    시작아이템 리소스 추가
    시작아이템 리소스 추가

---
+++ Assets/LeeDeaWoon/Script/AttackPattern/AttackPattern.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPattern : MonoBehaviour
+{
+    public List<GameObject> attackPrefab = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
