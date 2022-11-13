f94e78527ddb0bbe4d1327009cefc8fb9784b028
wtsiwon <siwon4727@gmail.com>
2022-11-13 15:50:45 +0900

    장애물 스트라이트 변경

--- Assets/Siwon/Script/Singletons/GameManager.cs
+++ Assets/Siwon/Script/Singletons/GameManager.cs
@@ -139,6 +139,7 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
     private IEnumerator CSetGame()
     {
         BackGroundSpawner.Instance.backgroundSpd = STARTSPD;
