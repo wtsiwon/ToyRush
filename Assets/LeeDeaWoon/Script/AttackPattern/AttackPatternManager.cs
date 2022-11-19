6d7938d67ad62dbc2519b097fca454b9f9ea6eb8
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-11-20 04:14:21 +0900

    공격패턴 중복제외 처리
    공격패턴 중복제외 처리

--- Assets/LeeDeaWoon/Script/AttackPattern/AttackPatternManager.cs
+++ Assets/LeeDeaWoon/Script/AttackPattern/AttackPatternManager.cs
@@ -8,12 +8,17 @@ public class AttackPatternManager : MonoBehaviour
     private void Awake() => inst = this;
 
     public List<GameObject> attackList = new List<GameObject>();
+    List<GameObject> attackStart = new List<GameObject>();
+
     public bool isAttackSummon;
     public int coolTime;
     public int attackDel;
 
     void Start()
     {
+        for (int i = 0; i < attackList.Count; i++)
+            attackStart.Add(attackList[i]);
+
         StartCoroutine(Attack_Spawn());
     }
 
@@ -23,16 +28,24 @@ public class AttackPatternManager : MonoBehaviour
 
     IEnumerator Attack_Spawn()
     {
-        // 소환시간 약 20초로 해놓기
+
+
         while (true)
         {
             if (GameManager.Instance.IsGameStart == true)
             {
+                if (attackList.Count == 0)
+                {
+                    for (int i = 0; i < attackStart.Count; i++)
+                        attackList.Add(attackStart[i]);
+                }
+                int attckRandom = Random.Range(0, attackList.Count);
                 yield return new WaitForSeconds(coolTime);
                 ObstacleSpawner.Instance.canSpawn = false;
 
                 yield return new WaitForSeconds(attackDel);
-                Instantiate(attackList[Random.Range(0, attackList.Count)], transform.position, Quaternion.identity).transform.parent = gameObject.transform;
+                Instantiate(attackList[attckRandom], transform.position, Quaternion.identity).transform.parent = gameObject.transform;
+                attackList.RemoveAt(attckRandom);
                 yield return new WaitForSeconds(3);
                 ObstacleSpawner.Instance.canSpawn = true;
             }
